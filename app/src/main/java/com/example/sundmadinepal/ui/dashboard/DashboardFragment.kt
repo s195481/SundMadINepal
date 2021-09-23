8be1a9117fc2e04affb22a07632dc4a9d3656ba9
Sejr_Yoga <s195481@student.dtu.dk>
2021-09-23 09:18:09 +0200

    Code cleanup

--- app/src/main/java/com/example/sundmadinepal/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/sundmadinepal/ui/dashboard/DashboardFragment.kt
@@ -21,12 +21,12 @@ class DashboardFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
