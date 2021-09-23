0506a05bcd8868c146d978336d6eafa09330781d
Sejr_Yoga <s195481@student.dtu.dk>
2021-09-23 09:21:55 +0200

    small comment edit

--- app/src/main/java/com/example/sundmadinepal/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/sundmadinepal/ui/dashboard/DashboardFragment.kt
@@ -16,8 +16,7 @@ class DashboardFragment : Fragment() {
     private lateinit var dashboardViewModel: DashboardViewModel
     private var _binding: FragmentDashboardBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
