ea58a764412bc1e2dab6e9ac9f57834aebe142d1
SilasRindorf <s195474@student.dtu.dk>
2021-11-04 15:29:03 +0100

    Added models

---
+++ app/src/main/java/com/example/sundmadinepal/model/model/Comics.kt
@@ -0,0 +1,6 @@
+package com.example.sundmadinepal.model.model
+
+data class Comics (
+    val comic_name: String,
+    val comic_pictures: List<String>
+)
---
+++ app/src/main/java/com/example/sundmadinepal/model/model/GoldenDays.kt
@@ -0,0 +1,7 @@
+package com.example.sundmadinepal.model.model
+
+data class GoldenDays (
+    val goldenDayPeriod: String,
+    val goldenDayTex: String,
+    val goldenDayPicture: String
+)
