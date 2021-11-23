f92a1f7357a57c9a2a31cb374a40275314c8be8d
Sejr_Yoga <s195481@student.dtu.dk>
2021-11-23 14:11:28 +0100

    Deleted almost anything ¯\_(ツ)_/¯ Ready for jetpack compose ¯\_(ツ)_/¯

--- app/src/main/java/com/example/sundmadinepal/model/model/Health.kt
+++ app/src/main/java/com/example/sundmadinepal/model/model/Health.kt
@@ -2,10 +2,10 @@ package com.example.sundmadinepal.model.model
 
 import java.util.*
 
-data class Health (
+data class Health(
     val child_name: String,
     val child_weight: Int,
     val child_height: Int,
     val child_birthdate: Date,
     val child_diary: String
-    )
+)
