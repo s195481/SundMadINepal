d80977d30df95ecfc121b5deb308c29b1a3cc966
Sejr_Yoga <s195481@student.dtu.dk>
2021-11-25 15:15:20 +0100

    ServiceLocator.init(this) added in App

--- app/src/main/java/com/example/sundmadinepal/App.kt
+++ app/src/main/java/com/example/sundmadinepal/App.kt
@@ -1,12 +1,14 @@
 package com.example.sundmadinepal
 
 import android.app.Application
+import dk.shortcut.dtudemoapp.di.ServiceLocator
 import java.util.*
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        ServiceLocator.init(this)
         Locale.setDefault(Locale("da", "DK"))
     }
 }
