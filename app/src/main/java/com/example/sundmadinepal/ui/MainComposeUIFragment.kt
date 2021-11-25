7685ec6b3cac1b7e499922a3d2009fa0ff07c8d1
Sejr_Yoga <s195481@student.dtu.dk>
2021-11-25 14:33:12 +0100

    Added images and size fixing to the homescreen

--- app/src/main/java/com/example/sundmadinepal/ui/MainComposeUIFragment.kt
+++ app/src/main/java/com/example/sundmadinepal/ui/MainComposeUIFragment.kt
@@ -1,10 +1,12 @@
 package com.example.sundmadinepal.ui
 
 import android.os.Bundle
+import android.util.Size
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -17,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
@@ -43,23 +46,26 @@ fun Greeting(name: String) {
 
 @Composable
 fun MainScreen() {
-    val navIconSize: Int = 200
+    val navIconSize: Int = 170
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxSize()
-
     ) {
         Row(
-            horizontalArrangement = Arrangement.Center
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
         ) {
             Icon(
-                painter = painterResource(id = R.drawable.temp),
-                contentDescription = "Temp",
-                modifier = Modifier.size(20.dp)
+                painter = painterResource(id = R.drawable.home),
+                contentDescription = "Home",
+                modifier = Modifier.size(50.dp).apply{padding(50.dp)}
             )
             Text(
-                text = "Home"
+                text = "Home",
+                modifier = Modifier.size(50.dp).apply{padding(50.dp)},
+                textAlign = TextAlign.Center
             )
         }
         Row(
@@ -70,7 +76,7 @@ fun MainScreen() {
 
                 }) {
                 Icon(
-                    Icons.Filled.ArrowBack,
+                    painter = painterResource(id = R.drawable.newborn),
                     "1000GoldenDays",
                     tint = Color.Magenta,
                     modifier = Modifier.size(navIconSize.dp)
@@ -81,7 +87,7 @@ fun MainScreen() {
 
                 }) {
                 Icon(
-                    Icons.Filled.ArrowBack,
+                    painter = painterResource(id = R.drawable.recipes),
                     "Recipes",
                     tint = Color.LightGray,
                     modifier = Modifier.size(navIconSize.dp)
@@ -94,7 +100,7 @@ fun MainScreen() {
 
                 }) {
                 Icon(
-                    Icons.Filled.ArrowBack,
+                    painter = painterResource(id = R.drawable.comic),
                     "Comics",
                     tint = Color.Yellow,
                     modifier = Modifier.size(navIconSize.dp)
@@ -105,7 +111,7 @@ fun MainScreen() {
 
                 }) {
                 Icon(
-                    Icons.Filled.ArrowBack,
+                    painter = painterResource(id = R.drawable.quiz),
                     "Quiz",
                     tint = Color.Green,
                     modifier = Modifier.size(navIconSize.dp)
@@ -118,7 +124,7 @@ fun MainScreen() {
 
                 }) {
                 Icon(
-                    Icons.Filled.ArrowBack,
+                    painter = painterResource(id = R.drawable.health_post),
                     "HealthPost",
                     tint = Color.Cyan,
                     modifier = Modifier.size(navIconSize.dp)
@@ -129,7 +135,7 @@ fun MainScreen() {
 
                 }) {
                 Icon(
-                    Icons.Filled.ArrowBack,
+                    painter = painterResource(id = R.drawable.baby),
                     "Health",
                     tint = Color.Red,
                     modifier = Modifier.size(navIconSize.dp)
