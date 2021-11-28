1dd789fe68088fa7b1c8a52b8b0944c9515a0e55
Sejr Lykke Björgheim Abildgaard <s195481@dtu.dk>
2021-11-28 11:25:44 +0100

    PC is dying. Moving work to the laptop

--- app/src/main/java/com/example/sundmadinepal/ui/recipe/RecipeComposeUIFragment.kt
+++ app/src/main/java/com/example/sundmadinepal/ui/recipe/RecipeComposeUIFragment.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import com.example.sundmadinepal.ui.theme.SundMadINepalTheme
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme.typography
+import com.example.sundmadinepal.model.model.Recipe
+import com.example.sundmadinepal.ui.recipe.RecipeViewModel.DataProvider.recipeList
 
 class GoldenDaysComposeUIFragment : Fragment() {
     override fun onCreateView(
@@ -21,62 +30,37 @@ class GoldenDaysComposeUIFragment : Fragment() {
     ): View? {
         return ComposeView(requireContext()).apply {
             setContent {
-                Text(text = "Hello world.")
+                Recipes()
+
             }
         }
     }
 }
 
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-/*
-@Composable
-fun MyScreen() {
-    var textState1 by remember { mutableStateOf("") }
-    var textState2 by remember { mutableStateOf("") }
-    Column() {
-        TextField(
-            value = textState1,
-            onValueChange = {
-                Log.d("textState1:", "textState1: $it")
-                textState1 = it
-            },
-            Modifier.padding(6.dp),
-            colors = TextFieldDefaults.outlinedTextFieldColors(backgroundColor = Color.Red)
-        )
-
-        TextField(
-            value = textState2,
-            onValueChange = {
-                Log.d("textState2:", "textState2: $it")
-                textState2 = it
-            },
-            Modifier.padding(6.dp),
-            colors = TextFieldDefaults.outlinedTextFieldColors(backgroundColor = Color.Blue)
-        )
-
-        Button(
-            onClick = {
-                Log.d("MyScreenButton", "MyScreenButton: $textState1, $textState2")
-            },
-            modifier = Modifier.scale(1.5f)
-        ) {
-            Text("Click me!")
-        }
-    }
-}*/
-@Composable
-fun MyScreen(viewModel: RecipeViewModel) {
-}
-
 
 @Composable
 fun Recipes(/* Can't remember what these guys are for "state: String, onValueChange: (String) -> Unit"*/) {
+    val puppies = remember { RecipeViewModel.DataProvider.recipeList }
+    LazyColumn(
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        items(
+            items = puppies,
+            itemContent = {
+                RecipeListItem(recipe = it)
+            })
+    }
+}
 
+@Composable
+fun RecipeListItem(recipe: Recipe) {
+    Row {
+        Column {
+            Text(text = recipe.name, style = typography.h6)
+            Text(text = recipe.id, style = typography.caption)
+        }
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/sundmadinepal/ui/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/example/sundmadinepal/ui/recipe/RecipeViewModel.kt
@@ -3,11 +3,58 @@ package com.example.sundmadinepal.ui.recipe
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.sundmadinepal.model.model.Recipe
 
 class RecipeViewModel : ViewModel() {
+    object DataProvider {
+        val recipeList = listOf(
+        Recipe(
+        id = "Dahl_v1",
+        name = "Dahl_v1",
+        ingredients = "Beans 1",
+        procedure = "Cook 1",
+        picture = ""
+        ),
+        Recipe(
+        id = "Dahl_v2",
+        name = "Dahl_v2",
+        ingredients = "Beans 2",
+        procedure = "Cook 2",
+        picture = ""
+        ),
+        Recipe(
+        id = "Dahl_v3",
+        name = "Dahl_v3",
+        ingredients = "Beans 3",
+        procedure = "Cook 3",
+        picture = ""
+        ),
+        Recipe(
+        id = "Dahl_v4",
+        name = "Dahl_v4",
+        ingredients = "Beans 4",
+        procedure = "Cook 4",
+        picture = ""
+        ),
+        Recipe(
+        id = "Dahl_v5",
+        name = "Dahl_v5",
+        ingredients = "Beans 5",
+        procedure = "Cook 5",
+        picture = ""
+        ),
+        Recipe(
+        id = "Dahl_v6",
+        name = "Dahl_v6",
+        ingredients = "Beans 6",
+        procedure = "Cook 6",
+        picture = ""
+        ))
+    }
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+    val recipe: Recipe = Recipe("Dahl_v1","Dahl","Beans","Cook","")
 }
