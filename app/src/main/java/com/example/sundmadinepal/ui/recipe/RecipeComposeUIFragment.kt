f172255fe2c4cfd7409eac5a31b91be4f9b47bfe
Sejr Lykke Björgheim Abildgaard <s195481@dtu.dk>
2021-11-28 14:50:13 +0100

    Recyclerview Recipes working visually getting info from viewmodel

--- app/src/main/java/com/example/sundmadinepal/ui/recipe/RecipeComposeUIFragment.kt
+++ app/src/main/java/com/example/sundmadinepal/ui/recipe/RecipeComposeUIFragment.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
@@ -19,7 +18,16 @@ import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import com.example.sundmadinepal.ui.theme.SundMadINepalTheme
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import com.example.sundmadinepal.R
 import com.example.sundmadinepal.model.model.Recipe
 import com.example.sundmadinepal.ui.recipe.RecipeViewModel.DataProvider.recipeList
 
@@ -41,24 +49,60 @@ class RecipeComposeUIFragment : Fragment() {
 
 @Composable
 fun Recipes(/* Can't remember what these guys are for "state: String, onValueChange: (String) -> Unit"*/) {
-    val recipies = remember { RecipeViewModel.DataProvider.recipeList }
-    LazyColumn(
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
-    ) {
-        items(
-            items = recipies,
-            itemContent = {
-                RecipeListItem(recipe = it)
-            })
+    val recipes = remember { RecipeViewModel.DataProvider.recipeList }
+    Column() {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.recipes),
+                contentDescription = "Recipes",
+                modifier = Modifier.size(50.dp).apply { padding(50.dp) }
+            )
+            Text(
+                text = "Recipes",
+                //modifier = Modifier.size(50.dp).apply { padding(50.dp) },
+                textAlign = TextAlign.Center
+            )
+        }
+        LazyColumn(
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+        ) {
+            items(
+                items = recipes,
+                itemContent = {
+                    RecipeListItem(recipe = it)
+                })
+        }
     }
 }
 
 @Composable
 fun RecipeListItem(recipe: Recipe) {
-    Row {
-        Column {
-            Text(text = recipe.name, style = typography.h6)
-            Text(text = recipe.id, style = typography.caption)
+    Column(
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier.
+        fillMaxSize().
+        padding(8.dp).
+        clip(RoundedCornerShape(corner = CornerSize(16.dp)))
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth().
+            background(Color.LightGray)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.settings),
+                contentDescription = recipe.id,
+                modifier = Modifier.size(50.dp).apply { padding(70.dp) }
+            )
+                Text(text = recipe.name, style = typography.h6)
+                Text(text = recipe.id, style = typography.caption)
+
         }
     }
 }
