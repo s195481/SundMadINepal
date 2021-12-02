d539f34c575972ab54bb8868bf7805112eb6483b
Sejr_Yoga <s195481@student.dtu.dk>
2021-12-02 16:59:59 +0100

    Commented out DB

--- app/src/main/java/com/example/sundmadinepal/App.kt
+++ app/src/main/java/com/example/sundmadinepal/App.kt
@@ -11,9 +11,9 @@ class App : Application() {
         super.onCreate()
         Locale.setDefault(Locale("da", "DK"))
         ServiceLocator.init(this)
-        ServiceLocator.db.RecipeGeneratorThing()
-        val list = ServiceLocator.db.recipeDao().loadAll()
-        Log.e("DB_Check", list[0].toString())
+        //ServiceLocator.db.RecipeGeneratorThing()
+        //val list = ServiceLocator.db.recipeDao().loadAll()
+        //Log.e("DB_Check", list[0].toString())
 
         Log.d("HERE", "I AM HERE")
     }
