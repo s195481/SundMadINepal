4ea74ccfaa0a3a047d72cb28ad3013c57a9188f1
SilasRindorf <s195474@student.dtu.dk>
2021-12-02 14:37:46 +0100

    Print out update

--- app/src/main/java/com/example/sundmadinepal/App.kt
+++ app/src/main/java/com/example/sundmadinepal/App.kt
@@ -12,7 +12,7 @@ class App : Application() {
         Locale.setDefault(Locale("da", "DK"))
         ServiceLocator.init(this)
         ServiceLocator.db.RecipeGeneratorThing()
-        println(ServiceLocator.db.recipeDao().loadById("Dahl_v1"))
+        Log.e("Check", ServiceLocator.db.recipeDao().loadById("Dahl_v1").toString())
         Log.d("HERE", "I AM HERE")
     }
 }
