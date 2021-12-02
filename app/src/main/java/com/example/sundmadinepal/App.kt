a42444ed0eb7cd522141568d886273b20ec01cc0
Sejr_Yoga <s195481@student.dtu.dk>
2021-12-02 16:54:50 +0100

    Commit for pull

--- app/src/main/java/com/example/sundmadinepal/App.kt
+++ app/src/main/java/com/example/sundmadinepal/App.kt
@@ -3,6 +3,11 @@ package com.example.sundmadinepal
 import android.app.Application
 import android.util.Log
 import com.example.sundmadinepal.di.ServiceLocator
+import com.example.sundmadinepal.model.db.RecipeEntity
+import com.example.sundmadinepal.ui.healthPost.temp
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.onEach
+
 import java.util.*
 
 class App : Application() {
@@ -12,7 +17,10 @@ class App : Application() {
         Locale.setDefault(Locale("da", "DK"))
         ServiceLocator.init(this)
         ServiceLocator.db.RecipeGeneratorThing()
-        Log.e("DB_Check", ServiceLocator.db.recipeDao().loadById("Dahl_v1").toString())
+        val list = ServiceLocator.db.recipeDao().loadById("Dahl_v1")
+        list[0]
+
         Log.d("HERE", "I AM HERE")
+
     }
 }
